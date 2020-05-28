bcaba67346dcca4d97d6fca08c9bb0e74471e7bc
selvasuriya <selvasuriya001@gmail.com>
2020-05-28 08:09:33 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,3 @@
-const myHeading = document.querySelector('h3');
-myHeading.textContent = canidrink(20);
-
-function multiply(num1, num2) {
-  let result = num1 * num2;
-  return result;
-}
-
-function canidrink(age) {
-  if(age > 20) {
-    return 'yep';
-  }
-  else {
-    return 'nope';
-  }
-}
-
 const constraints = {
   'video': true,
   'audio': true
